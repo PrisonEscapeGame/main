f356bb2a83d15584344d9db7c485c7f5ceb32038
temonn <t1poti00@students.oamk.fi>
2023-11-10 14:48:52 +0200

    Added a pause menu

--- Main.gd
+++ Main.gd
@@ -1,10 +1,25 @@
 extends Node
 
+var paused = false
+
 func play_game():
 	$Player2.start($StartPosition.position)
 	$Guard2.start($StartPosition.position)
 	$StartTimer.start()
 
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+
+func pauseMenu():
+	if paused:
+		$PauseMenu.hide()
+		Engine.time_scale = 1
+	else:
+		$PauseMenu.show()
+		Engine.time_scale = 0
+	
+	paused = !paused
 
 func _on_start_timer_timeout():
 	pass # Replace with function body.adada
