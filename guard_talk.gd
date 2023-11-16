6f202c717d31b92e8f23b4b43b460bf3037d3ed1
temonn <t1poti00@students.oamk.fi>
2023-11-16 14:38:20 +0200

    Player can now activate chat with guard

--- guard_talk.gd
+++ guard_talk.gd
@@ -4,6 +4,7 @@ extends Area2D
 func _input(event):
 	if event.is_action_pressed("ui_accept") and len(get_overlapping_bodies()) > 0:
 		use_dialogue()
+		print(get_overlapping_bodies())
 
 func use_dialogue():
 	var dialogue = get_parent().get_node("Dialogue")
