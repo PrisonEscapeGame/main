2ff338541af096ae23a2a906f29a32fdea878866
temonn <t1poti00@students.oamk.fi>
2023-11-22 13:34:51 +0200

    Small fix when opening the dialogue

--- guard_talk.gd
+++ guard_talk.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 func _input(event):
-	if event.is_action_pressed("ui_accept") and len(get_overlapping_bodies()) > 0:
+	if event.is_action_pressed("ui_accept") and len(get_overlapping_bodies()) > 1:
 		use_dialogue()
 		print(get_overlapping_bodies())
 
