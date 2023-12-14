2ceafc59a718892a47aed44f756ca976cbc7ba65
temonn <t1poti00@students.oamk.fi>
2023-12-14 12:55:28 +0200

    Ending screen now shows

--- ExitDoor.gd
+++ ExitDoor.gd
@@ -1,8 +1,13 @@
 extends Sprite2D
 
+@onready var ending = $"../You WON!"
+
 # When variable named value from PinLineEdit.gd is provided and is equal to global variable pinCode
 # the collision of the door will be disabled and the player is able to exit the map.
 func _physics_process(delta):
 	if global.userInput == global.pinCode:
 		$StaticBody2D/DoorCollisionShape.disabled = true
+		ending.show()
+		Engine.time_scale = 0
+		
 
